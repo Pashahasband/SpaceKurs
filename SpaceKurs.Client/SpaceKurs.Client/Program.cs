5caef911681d157ef6446e3d1c3fd3edf05fc9cd
Pashahasband <pashajurev@mail.ru>
2016-02-11 21:09:42 +0300

    Вынес всё что до этого делал в отдельные решения, очистив основные для нормального кода

--- SpaceKurs.Client/SpaceKurs.Client/Program.cs
+++ SpaceKurs.Client/SpaceKurs.Client/Program.cs
@@ -12,104 +12,7 @@ using System.Text;
 using System.Xml.Serialization;
 namespace SpaceKurs.Client
 {
-    public class UdpFileClient
-    {
-        // Детали файла
-        [Serializable]
-        public class FileDetails
-        {
-            public string FILETYPE = "";
-            public long FILESIZE = 0;
-        }
-
-        private static FileDetails fileDet;
-
-        // Поля, связанные с UdpClient
-        private static int localPort = 5002;
-        private static UdpClient receivingUdpClient = new UdpClient(localPort);
-        private static IPEndPoint RemoteIpEndPoint = null;
-
-        private static FileStream fs;
-        private static Byte[] receiveBytes = new Byte[0];
-
-        private static void GetFileDetails()
-        {
-            try
-            {
-                Console.WriteLine("-----------*******Ожидание информации о файле*******-----------");
-
-                // Получаем информацию о файле
-                receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
-                Console.WriteLine("----Информация о файле получена!");
-
-                XmlSerializer fileSerializer = new XmlSerializer(typeof(FileDetails));
-                MemoryStream stream1 = new MemoryStream();
-
-                // Считываем информацию о файле
-                stream1.Write(receiveBytes, 0, receiveBytes.Length);
-                stream1.Position = 0;
-
-                // Вызываем метод Deserialize
-                fileDet = (FileDetails)fileSerializer.Deserialize(stream1);
-                Console.WriteLine("Получен файл типа ." + fileDet.FILETYPE +
-                    " имеющий размер " + fileDet.FILESIZE.ToString() + " байт");
-            }
-            catch (Exception eR)
-            {
-                Console.WriteLine(eR.ToString());
-            }
-        }
-
-        public static void ReceiveFile()
-        {
-            try
-            {
-                Console.WriteLine("-----------*******Ожидайте получение файла*******-----------");
-
-                // Получаем файл
-                receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
-
-                // Преобразуем и отображаем данные
-                Console.WriteLine("----Файл получен...Сохраняем...");
-
-                // Создаем временный файл с полученным расширением
-                fs = new FileStream("temp." + fileDet.FILETYPE, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
-                fs.Write(receiveBytes, 0, receiveBytes.Length);
-
-                Console.WriteLine("----Файл сохранен...");
-
-                Console.WriteLine("-------Открытие файла------");
-
-                // Открываем файл связанной с ним программой
-                Process.Start(fs.Name);
-            }
-            catch (Exception eR)
-            {
-                Console.WriteLine(eR.ToString());
-            }
-            finally
-            {
-                fs.Close();
-                receivingUdpClient.Close();
-                Console.Read();
-            }
-        }
-
-        [STAThread]
-        static void Main(string[] args)
-        {
-            /*Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            */
-            // Получаем информацию о файле
-            GetFileDetails();
-
-            // Получаем файл
-            ReceiveFile();
-        }
-    }
-        /*static class Program
+    static class Program
     {
         /// <summary>
         /// Главная точка входа для приложения.
@@ -121,5 +24,5 @@ namespace SpaceKurs.Client
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
-    }*/
+    }
 }
