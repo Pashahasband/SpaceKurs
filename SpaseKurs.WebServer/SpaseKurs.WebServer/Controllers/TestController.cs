c82b7f111ab730850c46c0e3c5477d8ae3da1f72
Georgy Polikarpov <egorogor@mail.ru>
2016-04-02 16:44:32 +0300

    Еще один раз проект с SignalR

---
+++ SpaseKurs.WebServer/SpaseKurs.WebServer/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SpaseKurs.WebServer.Controllers
+{
+    [RoutePrefix("test")]
+    public class TestController : ApiController
+    {
+        [Route("hello")]
+        public string GetHello()
+        {
+            return "Hello!";
+        }
+    }
+}
---
+++ SpaseKurs.WebServer/SpaseKurs.WebServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpaseKurs.WebServer.WebApiApplication" Language="C#" %>
