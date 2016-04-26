e385e6b109c499f507421b59fe5dab712edc5184
Georgy Polikarpov <g.polikarpov@ts-soft.ru>
2016-04-26 12:18:50 +0300

    Контроллер должен теперь уметь отдавать файл по индексу

--- SpaceKurs.Server/SpaceKurs.Server/ImagesController.cs
+++ SpaceKurs.Server/SpaceKurs.Server/ImagesController.cs
@@ -1,32 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
-
-namespace SpaceKurs.Server
+﻿namespace SpaceKurs.Server
 {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Web.Http;
+
     public class ImagesController : ApiController
     {
         public HttpResponseMessage Get()
         {
-            //using (var fileStream = new FileStream("D:\\lena.jpg", FileMode.Open))
-            //{
-            //    return fileStream;
-            //}
-
             HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StreamContent(new FileStream("lena.jpg", FileMode.Open, FileAccess.Read));
-            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
-            response.Content.Headers.ContentDisposition.FileName = "lena.jpg";
-            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/jpg");
+            using (var fileStream = new FileStream("lena.jpg", FileMode.Open, FileAccess.Read))
+            {
+                response.Content = new StreamContent(fileStream);
+            }
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                                                          {
+                                                              FileName = "lena.jpg"
+                                                          };
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
 
             return response;
-            //return "ImagesContoller works!";
+        }
+
+        public HttpResponseMessage Get(
+            int id)
+        {
+            var imagePath = Program.Images.ElementAtOrDefault(id);
+            if (imagePath == null)
+            {
+                throw new Exception(string.Format("Image with id={0} does not exist", id));
+            }
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            using (var fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+            {
+                response.Content = new StreamContent(fileStream);
+            }
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                                                          {
+                                                              FileName = string.Format("{0}.jpg", id),
+                                                          };
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
+
+            return response;
         }
     }
 }
