f8abbbdfaaeba8b553dbaaab765d8202e9a5dd84
Georgy <egorogor@mail.ru>
2016-04-27 19:08:58 +0300

    Контроллер для превьюшек

--- SpaceKurs.Server/SpaceKurs.Server/PreviewsController.cs
+++ SpaceKurs.Server/SpaceKurs.Server/PreviewsController.cs
@@ -1,15 +1,19 @@
-﻿namespace SpaceKurs.Server
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceKurs.Server
 {
-    using System;
     using System.IO;
-    using System.Linq;
     using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Web.Http;
 
-    [RoutePrefix("api/images")]
-    public class ImagesController : ApiController
+    [RoutePrefix("/api/previews")]
+    public class PreviewsController : ApiController
     {
         [HttpGet]
         public HttpResponseMessage Get()
@@ -20,16 +24,16 @@
                 response.Content = new StreamContent(fileStream);
             }
             response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
-                                                          {
-                                                              FileName = "lena.jpg"
-                                                          };
+            {
+                FileName = "lena.jpg"
+            };
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
 
             return response;
         }
 
+        [Route("{id:guid")]
         [HttpGet]
-        [Route("{id:guid}")]
         public HttpResponseMessage Get(
             Guid id)
         {
@@ -40,14 +44,14 @@
             }
 
             HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-            using (var fileStream = new FileStream(imageInfo.ImagePath, FileMode.Open, FileAccess.Read))
+            using (var fileStream = new FileStream(imageInfo.PreviewPath, FileMode.Open, FileAccess.Read))
             {
                 response.Content = new StreamContent(fileStream);
             }
             response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
-                                                          {
-                                                              FileName = string.Format("{0}.jpg", id),
-                                                          };
+            {
+                FileName = string.Format("{0}.jpg", id),
+            };
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
 
             return response;
